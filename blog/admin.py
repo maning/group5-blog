d3bc98751cb72307965d749d9d1632eeeb2281ac
maning <maning@mapbox.com>
2019-11-26 20:10:56 +0800

    flake8 fixes

--- blog/admin.py
+++ blog/admin.py
@@ -3,11 +3,13 @@ from .models import Post, Comment
 
 # Register your models here.
 
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
